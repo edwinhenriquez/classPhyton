b36b74849236621e16082cf8c0978decfc10bd51
edwinhenriquez <dev.edwin.henriquez@gmail.com>
2018-04-17 23:33:39 +0000

    pruebas de variables y print

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print ("Hola Mundo")
+el_mundo_es_plano = True
+if el_mundo_es_plano:
+    print ("Tené cuidado de no caerte")
+    
+
+# este es el primer comentario
+
+spam = 1                 # y este es el segundo comentario
+                         # ... y ahora un tercero!
+text = "# Este no es un comentario, es un String"
+
+print (spam)
+print (text)
+
+print ("Como calculadora")
+print ("Sumar")
+valor1 = 6
+valor2 = 3
+print (valor1)
+ 
+print ("+")
+print (valor2)
+suma = valor1 + valor2
+print ("Igual")
+print (suma)
+print ("Por 2")
+multiplicacion = suma * 2
+print (multiplicacion)
+resta = (multiplicacion - 5)
+print ("menos 5")
+print (resta)
+division = round((resta / 3) ,2)
+print ("entre 3")
+print (division)
+print ("***** Metodos para String")
+nombre = 'alex'
+print (nombre)
+ininombre = (nombre[0])
+print (ininombre)
+print (nombre.upper())
+print (nombre.lower())
+print (nombre.title())
